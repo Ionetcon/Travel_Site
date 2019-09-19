2b52b1580a1f125ac766f0261f654f38be3605b6
Ionetcon <antarlietechnology@gmail.com>
2019-09-19 18:24:26 -0400

    Edited Gulp file for new version

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
 var gulp = require('gulp'),
 watch = require('gulp-watch');
 
-gulp.task('default', function() {
+gulp.task('default', async function() {
   console.log("Hooray - you created a Gulp task.");
 });
 
-gulp.task('html', function() {
+gulp.task('html', async function() {
   console.log("Imagine something useful being done to your HTML here.");
 });
 
-gulp.task('styles', function() {
+gulp.task('styles', async function() {
   console.log("Imagine Sass or PostCSS tasks running here.");
 });
 
